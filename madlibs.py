ad58a6fefeb7ca3ce7e9a801ebe292127daa7293
Ashland_S13 <agoett89@users.noreply.github.com>
2022-01-08 15:03:37 -0500

    Madlib program

---
+++ madlibs.py
@@ -0,0 +1,34 @@
+user = input("UserName: ")
+
+print(f"Follow @{user} for more Python Projects!")
+
+#madlib input variables
+
+silly_name = input("Enter Hero's Name: ") 
+job = input("Unrealistic Profession: ")
+noun = input("Noun: ") 
+verb= input("Verb: ")
+country = input("Country: ")
+silly_name2 = input("Silly Name2: ") 
+color = input("Color: ")
+adj = input("Adjective: ") 
+adverb = input("Adverb: ")
+silly_name3 = input("Silly Name3: ") 
+silly_name4 = input("Silly Name4: ") 
+face = input("Facial Feature: ") 
+us_city = input("US City: ") 
+final_silly = input("Silly Final Boss: ") 
+noun2 = input("Another Noun: ") 
+fmr_actor = input("Formerly Badass Actor now selling out: ") 
+noun3 = input("Another Noun: ")
+
+
+#madlib
+madlib = f"Meet our hero {silly_name}, a super-intelligent {job}. A run-in with the {country} military leads him to create his \
+alter-ego {silly_name2}, a {color} {adj} giant capable of great destruction. He {adverb} battles the military with his \
+girlfriend {silly_name3}. Eventually it is discovered that our hero's long time colleague {silly_name4}, distinguished \
+by his {face} is trying to turn {silly_name2} into a weapon, leading to a climactic (if pointless) battle in downtown \
+{us_city} with an evil version of the same giant alter-ego called {final_silly}. Eventually the enemy is subdued by {verb}ing\
+him with a {noun2}. In the final reel,{fmr_actor} appears to propose joining him in a {noun3}"
+
+print(madlib)
